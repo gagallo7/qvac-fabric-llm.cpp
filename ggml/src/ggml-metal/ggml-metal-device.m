a4fb98b08b2cb724d126c8e81fa130d447a88162
Korcan Hussein <korcan.hussein@collabora.com>
2026-08-10 11:32:01 +0100

    ggml-metal: allow ggml_metal_library_get_pipeline_* fns to derive thread dispatch sizes
    (cherry picked from commit 65f2cf93586f89c65782d33a8e4fc1c77a47f8bb)
    (cherry picked from commit 94d54c5f0cc32a1c1020a12d648f1d3ccf0c5baf)

--- ggml/src/ggml-metal/ggml-metal-device.m
+++ ggml/src/ggml-metal/ggml-metal-device.m
@@ -699,6 +699,7 @@ struct ggml_metal_pipeline_with_params ggml_metal_library_get_pipeline(ggml_meta
         /*.smem     =*/ 0,
         /*.c4       =*/ false,
         /*.cnt      =*/ false,
+        /*.nth      =*/ 0,
     };
 
     res.pipeline = ggml_metal_pipelines_get(lib->pipelines, name);
@@ -717,6 +718,7 @@ struct ggml_metal_pipeline_with_params ggml_metal_library_compile_pipeline(ggml_
         /*.smem     =*/ 0,
         /*.c4       =*/ false,
         /*.cnt      =*/ false,
+        /*.nth      =*/ 0,
     };
 
     [lib->lock lock];
