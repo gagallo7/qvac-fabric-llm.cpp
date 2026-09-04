6c1dc5b576c7b36c84351761a47cb5ffbd0fb0a1
Aman Gupta <aman.gupta@tether.io>
2026-09-04 17:59:24 +0800

    common : add --n-cpu-ffn option
    (cherry picked from commit e83210a0fbb92f42a5b0432590c98e2921be8719)

--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -5082,7 +5082,7 @@ static void ggml_backend_cuda_device_get_props(ggml_backend_dev_t dev, ggml_back
         /* .buffer_from_host_ptr  = */ false,
         /* .events                = */ events,
         /* .mmap_support          = */ props->type != GGML_BACKEND_DEVICE_TYPE_IGPU,
-        /* .copy_stream           = */ true,
+        /* .copy_stream           = */ events,
     };
 }
 
