dev-nid/qvac-ext-lib-llama.cpp

ggml : add q4 q8 vector index search
Assisted-by: GPT-5.5

ggml/src/ggml-vector-index-avx2.h
@@ -1,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ggml_vec_index_detail {
+
+float dot_q8_avx2(const float * query, const int8_t * codes, float scale, int dim);
+float dot_q4_avx2(const float * query, const uint8_t * codes, float scale, int dim);
+
+} // namespace ggml_vec_index_detail

ggml/src/ggml-vector-index-sources.cmake
@@ -1,0 +1,9 @@
+set(GGML_VECTOR_INDEX_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
+set(GGML_VECTOR_INDEX_IMPL_SOURCES
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index.cpp"
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-search.cpp"
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-persistence.cpp")
+
+set(GGML_VECTOR_INDEX_INTERNAL_HEADERS
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-internal.h")

gagallo7/qvac-fabric-llm.cpp

(cherry picked from commit e6dadc002aed515318f80d612f7dd1b4332cbbef)
b10549 squash: folded 12 commits.
- ggml : restore vector-index utility APIs
- ggml : reject trailing vector-index snapshots
- docs : expand vector-index component map
- ggml : fix AVX2 vector-index q4 q8 search build Use an AVX2-compatible sign-extension sequence for q4/q8 search and add focused quantized reference and IVF recall coverage.
- ggml : fix vector-index q4/q8 search issues Harden quantized scoring, IVF probing, remove semantics, and vector-index packaging while moving the private build wiring into the vector-index subdirectory.
- ggml : harden vector index persistence
- ggml : fix vector-index all_finite linkage
- ggml : fix vector-index helper linkage Keep file-local capacity helpers internal so fatal-warning builds do not reject them for missing prototypes.
- Ensure the standalone vector-index package links libm and includes it in Libs.private so static pkg-config consumers resolve fenv/math symbols.
- ggml : preserve vector index ranking precision
- ggml : fix vector-index test hook linkage
- ggml : fix vector-index PR2a review issues
Squashed-with: 7036c698736
Squashed-with: 0873f7a1bdf
Squashed-with: 8508e354c65
Squashed-with: c9fba523359
Squashed-with: 8402f1a86ca
Squashed-with: 70e61f80187
Squashed-with: 46bfbd36165
Squashed-with: 2104e172bae
Squashed-with: 33a1c935819
Squashed-with: dd7c03d61a7
Squashed-with: c27a625d00b
Squashed-with: d6f57096f2c
Assisted-by: Claude Fable 5

ggml/src/ggml-vector-index-avx2.h
@@ -1,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ggml_vec_index_detail {
+
+double dot_q8_avx2(const float * query, const int8_t * codes, float scale, int dim);
+double dot_q4_avx2(const float * query, const uint8_t * codes, float scale, int dim);
+
+} // namespace ggml_vec_index_detail

ggml/src/ggml-vector-index-sources.cmake
@@ -1,0 +1,9 @@
+set(GGML_VECTOR_INDEX_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
+set(GGML_VECTOR_INDEX_IMPL_SOURCES
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index.cpp"
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-search.cpp"
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-persistence.cpp")
+
+set(GGML_VECTOR_INDEX_INTERNAL_HEADERS
+    "${GGML_VECTOR_INDEX_SOURCE_DIR}/ggml-vector-index-impl.h")