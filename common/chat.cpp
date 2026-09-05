makaveli10/qvac-ext-lib-llama.cpp

common: Split jinja headers, add aux functions
TODO: commit-split the common/chat.cpp changes

Fix IQ1_S sharded model UAF and Linux Vulkan build, plus
create_split_backend_buffers now moves per-split ggml_contexts into
ml.ctx_map so create_backend_buffers can transfer ownership into
pimpl->ctxs_bufs, the bundled vulkan_cpp_wrapper include is now gated on
ANDROID to avoid clashing with the system vulkan.hpp on desktop, and the
existing common/chat thinking_forced_open + 2-arg
common_init_from_model_and_params overload.

Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

common/chat.cpp
@@ -2436,7 +2436,30 @@ static common_chat_params common_chat_templates_apply_jinja(const struct common_
         return data;
     }
 
+    // Mark thinking as "forced open" when the chat template's generation-prompt
+    // suffix leaves the reasoning section open.
+    auto thinking_forced_open_from_gen_prompt = [](const std::string & gen_prompt,
+                                                   const std::string & start_tag,
+                                                   const std::string & end_tag) -> bool {
+        if (start_tag.empty() || gen_prompt.empty()) {
+            return false;
+        }
+        const auto last_start = gen_prompt.rfind(start_tag);
+        if (last_start == std::string::npos) {
+            return false;
+        }
+        if (end_tag.empty()) {
+            return true;
+        }
+        const auto last_end = gen_prompt.rfind(end_tag);
+        return last_end == std::string::npos || last_end < last_start;
+    };
+
     if (auto result = common_chat_try_specialized_template(tmpl, src, params)) {
+        result->thinking_forced_open  = result->supports_thinking &&
+            thinking_forced_open_from_gen_prompt(result->generation_prompt,
+                                                 result->thinking_start_tag,
+                                                 result->thinking_end_tag);
         return *result;
     }
 
@@ -2463,6 +2486,21 @@ static common_chat_params common_chat_templates_apply_jinja(const struct common_
             auto_params.thinking_start_tag = trim_whitespace(autoparser.reasoning.start);
             auto_params.thinking_end_tag   = trim_whitespace(autoparser.reasoning.end);
         }
+        // Leave auto_params.generation_prompt as what generate_parser computed
+        // (character-level diff via common_chat_template_generation_prompt, plus
+        // any continuation-mode reasoning/content extensions). We previously
+        // overwrote this with params.generation_prompt (which uses the
+        // segment-based calculate_diff_split), but for templates like
+        // MiniMax-M2 whose role delimiters are bracket-shaped (`[e~[`, `]~b]`),
+        // segment-based diff includes the trailing `[e~[` of the prior message
+        // as part of the "right" diff. That makes auto_params.generation_prompt
+        // disagree with parser_generation_prompt (used to build the parser),
+        // and the PEG parser then fails on inputs that include the spurious
+        // delimiter prefix.
+        auto_params.thinking_forced_open = auto_params.supports_thinking &&
+            thinking_forced_open_from_gen_prompt(auto_params.generation_prompt,
+                                                 auto_params.thinking_start_tag,
+                                                 auto_params.thinking_end_tag);
         common_peg_arena arena;
         arena.load(auto_params.parser);
         LOG_DBG("%s: generated parser:\n%s\n\nparser generation prompt: %s\n", __func__, arena.dump(arena.root()).c_str(), auto_params.generation_prompt.c_str());

gagallo7/qvac-fabric-llm.cpp

restore the delete-lora backstop in ~llama_model; nulling lora->model instead leaked every adapter still registered at model teardown (training relies on this backstop, and llama_adapter_lora_free already erases freed adapters from the set)
b10297 rebase:
- thinking_forced_open_from_gen_prompt: upstream turned
  common_chat_params::thinking_end_tag into the thinking_end_tags list;
  treat thinking as closed when any end tag appears after the last
  start tag.
(cherry picked from commit 1fa65c328e83ce2dcfe64947d48d7955446260c1)

common/chat.cpp
@@ -3703,7 +3703,38 @@ static common_chat_params common_chat_templates_apply_jinja(const struct common_
         return data;
     }
 
+    // Mark thinking as "forced open" when the chat template's generation-prompt
+    // suffix leaves the reasoning section open.
+    auto thinking_forced_open_from_gen_prompt = [](const std::string & gen_prompt,
+                                                   const std::string & start_tag,
+                                                   const std::vector<std::string> & end_tags) -> bool {
+        if (start_tag.empty() || gen_prompt.empty()) {
+            return false;
+        }
+        const auto last_start = gen_prompt.rfind(start_tag);
+        if (last_start == std::string::npos) {
+            return false;
+        }
+        if (end_tags.empty()) {
+            return true;
+        }
+        for (const auto & end_tag : end_tags) {
+            if (end_tag.empty()) {
+                return true;
+            }
+            const auto last_end = gen_prompt.rfind(end_tag);
+            if (last_end != std::string::npos && last_end > last_start) {
+                return false;
+            }
+        }
+        return true;
+    };
+
     if (auto result = common_chat_try_specialized_template(tmpl, src, params)) {
+        result->thinking_forced_open  = result->supports_thinking &&
+            thinking_forced_open_from_gen_prompt(result->generation_prompt,
+                                                 result->thinking_start_tag,
+                                                 result->thinking_end_tags);
         return *result;
     }
 
@@ -3731,6 +3762,21 @@ static common_chat_params common_chat_templates_apply_jinja(const struct common_
                 auto_params.thinking_end_tags = {std::move(end_tag)};
             }
         }
+        // Leave auto_params.generation_prompt as what generate_parser computed
+        // (character-level diff via common_chat_template_generation_prompt, plus
+        // any continuation-mode reasoning/content extensions). We previously
+        // overwrote this with params.generation_prompt (which uses the
+        // segment-based calculate_diff_split), but for templates like
+        // MiniMax-M2 whose role delimiters are bracket-shaped (`[e~[`, `]~b]`),
+        // segment-based diff includes the trailing `[e~[` of the prior message
+        // as part of the "right" diff. That makes auto_params.generation_prompt
+        // disagree with parser_generation_prompt (used to build the parser),
+        // and the PEG parser then fails on inputs that include the spurious
+        // delimiter prefix.
+        auto_params.thinking_forced_open = auto_params.supports_thinking &&
+            thinking_forced_open_from_gen_prompt(auto_params.generation_prompt,
+                                                 auto_params.thinking_start_tag,
+                                                 auto_params.thinking_end_tags);
         common_peg_arena arena;
         arena.load(auto_params.parser);
         LOG_DBG("%s: generated parser:\n%s\n\nparser generation prompt: %s\n", __func__, arena.dump(arena.root()).c_str(), auto_params.generation_prompt.c_str());