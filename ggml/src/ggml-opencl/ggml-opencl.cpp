infinitalo/llama.cpp

log: add opencl memory debug messages

ggml/src/ggml-opencl/ggml-opencl.cpp
@@ -4041,6 +4041,9 @@ struct ggml_backend_opencl_buffer_context {
 
 static void ggml_backend_opencl_buffer_free_buffer(ggml_backend_buffer_t buffer) {
     ggml_backend_opencl_buffer_context * ctx = (ggml_backend_opencl_buffer_context *) buffer->context;
+    GGML_LOG_INFO("[DEBUG] opencl buf FREE  cl_mem=%p size=%.2f MiB\n",
+        (void *)(ctx->buffer.empty() ? nullptr : ctx->buffer[0]),
+        buffer->size / 1024.0 / 1024.0);
     delete ctx;
 }
 
@@ -5185,6 +5188,8 @@ static ggml_backend_buffer_t ggml_backend_opencl_buffer_type_alloc_buffer(ggml_b
     }
 
     ggml_backend_opencl_buffer_context * ctx = new ggml_backend_opencl_buffer_context(mem);
+    GGML_LOG_INFO("[DEBUG] opencl buf ALLOC cl_mem=%p size=%.2f MiB\n",
+        (void *)mem, size / 1024.0 / 1024.0);
 
     return ggml_backend_buffer_init(buffer_type, ggml_backend_opencl_buffer_interface, ctx, size);
 }

gagallo7/qvac-fabric-llm.cpp

Co-authored-by: Guilherme Gallo <guilherme.gallo@collabora.com>
---
b9692 rebase:
- Demote the buffer FREE/ALLOC message from LOG_INFO to LOG_DEBUG so it
  no longer spams normal-verbosity output.
remove leftover [DEBUG] instrumentation that logged VmRSS/VmSwap from /proc/self/status at INFO on every model teardown (non-portable, log spam)
(cherry picked from commit 5c33d64aeb0382f7c7cfc3156da49dd345823493)
(cherry picked from commit 70cda1b166532b2c2bed4f0537c20594c137ee37)

ggml/src/ggml-opencl/ggml-opencl.cpp
@@ -9271,6 +9271,9 @@ struct ggml_backend_opencl_buffer_context {
 
 static void ggml_backend_opencl_buffer_free_buffer(ggml_backend_buffer_t buffer) {
     ggml_backend_opencl_buffer_context * ctx = (ggml_backend_opencl_buffer_context *) buffer->context;
+    GGML_LOG_DEBUG("[DEBUG] opencl buf FREE  cl_mem=%p size=%.2f MiB\n",
+        (void *)(ctx->buffer.empty() ? nullptr : ctx->buffer[0]),
+        buffer->size / 1024.0 / 1024.0);
     delete ctx;
 }
 
@@ -12209,6 +12212,8 @@ static ggml_backend_buffer_t ggml_backend_opencl_buffer_type_alloc_buffer(ggml_b
     }
 
     ggml_backend_opencl_buffer_context * ctx = new ggml_backend_opencl_buffer_context(mem);
+    GGML_LOG_DEBUG("[DEBUG] opencl buf ALLOC cl_mem=%p size=%.2f MiB\n",
+        (void *)mem, size / 1024.0 / 1024.0);
 
     return ggml_backend_buffer_init(buffer_type, ggml_backend_opencl_buffer_interface, ctx, size);
 }